9099900dcf8ef380c792e5d85cf64f6017f8a102
bakhshi <esfand.esfandy555@gmail.com>
2023-04-25 20:36:28 +0430

    version 4

---
+++ train.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hagwartz
+{
+    public class train
+    {
+        private string name;
+        private long capacity;
+        private DateTime departureTime;
+        
+    }
+}
