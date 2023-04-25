ef7d9bee85cce8684ac436afbc0ee29a3fb97fab
bakhshi <esfand.esfandy555@gmail.com>
2023-04-26 03:15:12 +0430

    version 7

--- admin.cs
+++ admin.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Hagwartz
 {
-    public class admin:authorizedPersons
+    public class admin : authorizedPersons
     {
         private List<dorm> dormsList = new List<dorm>();
 
@@ -15,7 +15,7 @@ namespace Hagwartz
 
         string login(string username, string password)
         {
-            if (username=="AliBakhshi" && password=="1382")
+            if (username == "AliBakhshi" && password == "1382")
             {
                 return ("login successfully");
             }
@@ -24,5 +24,32 @@ namespace Hagwartz
                 return ("Not found");
             }
         }
+
+        void sendInvitation()
+        {
+            Program.authorizedPersonsDetector();
+            for (int i = 0; i < Program.authorizedPersonsDetector().Length; i++)
+            {
+                if (Program.authorizedPersonsDetector()[i].Role == role.student)
+                {
+                    Program.authorizedPersonsDetector()[i].Letter.Add("Dear " + Program.authorizedPersonsDetector()[i].Name + " " + Program.authorizedPersonsDetector()[i].Family + ":" + "Welcome to Hagwartz");
+                }
+            }
+        }
+
+        void sendTicket(train _train)
+        {
+            Program.authorizedPersonsDetector();
+            for (int i = 0; i < Program.authorizedPersonsDetector().Length; i++)
+            {
+                if (Program.authorizedPersonsDetector()[i].Role == role.student)
+                {
+                    Program.authorizedPersonsDetector()[i].Letter.Add("train name:"+_train.Name+"      "+"departure time:"+_train.DepartureTime+"           "+"cabin number"+_train.CabinNumber+"          "+"chair number:"+_train.ChairNumber);
+                }
+
+            }
+        }
+
+        
     }
 }
--- train.cs
+++ train.cs
@@ -5,8 +5,39 @@ namespace Hagwartz
     public class train
     {
         private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
         private long capacity;
+
+        public long Capacity
+        {
+            get => capacity;
+            set => capacity = value;
+        }
+        private long cabinNumber;
+
+        public long CabinNumber
+        {
+            get => cabinNumber;
+            set => cabinNumber = value;
+        }
+        private long chairNumber;
+
+        public long ChairNumber
+        {
+            get => chairNumber;
+            set => chairNumber = value;
+        }
         private DateTime departureTime;
-        
+
+        public DateTime DepartureTime
+        {
+            get => departureTime;
+            set => departureTime = value;
+        }
     }
 }
